eacd9f7dce970c86ae4be696eb78526a7ff84a0f
haily <hailyna93@gmail.com>
2020-07-29 16:28:57 +0700

    intro page

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,3 +1,4 @@
+import 'package:covid_app/core/constant.dart';
 import 'package:flutter/material.dart';
 
 class IntroPage extends StatefulWidget {
@@ -8,8 +9,27 @@ class IntroPage extends StatefulWidget {
 class _IntroPageState extends State<IntroPage> {
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      
+      body: Container( 
+        width: size.width,
+        height: size.height,
+        decoration: BoxDecoration(  
+          gradient: LinearGradient(  
+            colors: [
+              AppColors.mainColor,
+              AppColors.mainColor.withOpacity(.5)
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter
+          ),
+        ),
+        child: Stack(  
+          children: <Widget>[
+            Image.asset("assets/images/logo.png")
+          ],
+        ),
+      ),
     );
   }
 }
